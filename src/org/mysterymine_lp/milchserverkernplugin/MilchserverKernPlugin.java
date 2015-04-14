75b51ddc7ef48cee06a9c02c3b1749c0c18fd001
LeonHD99 <LeonHenke@web.de>
2015-04-14 19:34:28 +0200

    Milchserver Kernplugin

---
+++ src/org/mysterymine_lp/milchserverkernplugin/MilchserverKernPlugin.java
@@ -0,0 +1,43 @@
+package org.mysterymine_lp.milchserverkernplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MilchserverKernPlugin extends JavaPlugin {
+	
+	public void onEnable() {
+		System.out.println("Milchserver Kernplugin aktiviert!");
+	}
+	
+	public void onDisable() {
+		System.out.println("Milchserver Kernplugin deaktiviert!");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandlabel,String[] args) {
+		Player player = (Player) sender;
+		boolean kern = true;
+		if(cmd.getName().equalsIgnoreCase("ping"))
+		{
+			player.sendMessage("Pong!");
+			kern = false;
+		}
+		if(cmd.getName().equalsIgnoreCase("heilen"))
+		{
+			if(player.isOp())
+			{
+				player.setHealth(20);
+				player.sendMessage("Geheilt!");
+				kern = false;
+			}
+			else;
+			{
+				player.sendMessage("§4Keine Berechtigung!");
+				kern = false;
+			}
+		}
+		return kern;
+	}
+
+}
